c7e7cb36cee41af7eff8d712f925511e5c35a298
Zachary Sasser <ztsasser@gmail.com>
2019-07-25 09:26:24 -0500

    Create findBias.c

---
+++ findBias.c
@@ -0,0 +1,22 @@
+#include <kipr/botball.h>
+
+int main()
+{
+    //Stop the mtoors (make bot stand still)
+    ao();
+    msleep(300);
+    
+    //Find the average noise
+    int i = 0;
+    double avg = 0;
+    while(i<60){
+     	avg += gyro_z();
+        msleep(1);
+        i++;
+    }
+    double bias = avg/50.0;
+    
+    //Print our bias
+   	printf("New Bias: %f \n", bias);
+    return 0;
+}
